4aa33ed3d688e6ae21ae96f63f670e2055842d0a
samogera <samuelohillary@gmail.com>
2024-04-29 09:12:43 +0300

    bank of flatiron

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import AccountContainer from "./AccountContainer";
 function App() {
     return (
       <div className="ui raised segment">
-        <div className="ui segment violet inverted">
+        <div className="ui segment grey inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
         <AccountContainer />
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -4,16 +4,15 @@ function TransactionList() {
   const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
-    // Fetch transactions from backend server
     fetch('http://localhost:8001/transactions')
       .then(response => response.json())
-      .then(data => setTransactions(data)) // Set data directly as transactions state
+      .then(data => setTransactions(data))
       .catch(error => console.error('Error fetching transactions:', error));
-  }, []); // Empty dependency array ensures this effect runs only once on component mount
+  }, []);
 
   return (
     <div>
-      <h2>Transaction List</h2>
+      <h2>Your Transactions</h2>
       <table>
         <thead>
           <tr>
